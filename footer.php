5e9c7fb2f930630e531e57cd2b21eef55a2fe325
Jonathan Reinink <jonathan@reinink.ca>
2015-05-28 10:04:23 -0400

    Minor tweak to site footer.

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@
 
 <div class="copyright">
     <div class="copyright"><p class="church">&copy; Copyright <?=date('Y')?> <?php bloginfo('blogname')?></p>
-    <p class="software">Powered by <a href="http://churchsocialapp.com" title="Church Management Software">Church Social</a></p></div>
+    <p class="software">Powered by <a href="http://churchsocialapp.com" title="Church Management Software">Church Social</a>.</p></div>
 </div>
 
 <script src="<?php bloginfo('template_url') ?>/js/all.min.js"></script>
