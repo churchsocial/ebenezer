ede15e6ff4f7d6139912606692e1e67bf1274424
Jonathan Reinink <jonathan@reinink.ca>
2016-03-22 18:42:19 -0400

    Install Laravel Elixir for asset compilation.

--- footer.php
+++ footer.php
@@ -32,7 +32,7 @@
     <p class="software">Powered by <a href="http://churchsocialapp.com" title="Church Management Software">Church Social</a>.</p></div>
 </div>
 
-<script src="<?php bloginfo('template_url') ?>/js/all.min.js?v=<?=wp_get_theme()->get('Version')?>"></script>
+<script src="<?php bloginfo('template_url') ?>/js/all.js?v=<?=wp_get_theme()->get('Version')?>"></script>
 
 <?php wp_footer() ?>
 
